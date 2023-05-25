fe6b745e3ab90dccfcb9e312fc555885191f1839
MrDesilvestri <124096886+MrDesilvestri@users.noreply.github.com>
2023-05-25 16:43:28 -0500

    Update Spring Inicializer

---
+++ develpo/src/main/java/com/example/develpo/DevelpoApplication.java
@@ -0,0 +1,13 @@
+package com.example.develpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelpoApplication.class, args);
+	}
+
+}
---
+++ develpo/src/test/java/com/example/develpo/DevelpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.develpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
